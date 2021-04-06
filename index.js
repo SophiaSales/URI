32cdbc8e7cf49aca28287aab78befe18c90f85d1
SophiaSales <sophiabrenda10@gmail.com>
2021-04-06 18:56:48 -0300

    second day started

---
+++ index.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+console.log( typeof lines);
