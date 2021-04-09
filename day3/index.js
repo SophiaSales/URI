d7deaef1d123378c6d420fa5a9a84b5a5ee3a2b4
SophiaSales <sophiabrenda10@gmail.com>
2021-04-08 21:59:53 -0300

    day3 finished

---
+++ day3/index.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+
+var n = 3.14159;
+var raio = parseFloat(input);
+var area = n * Math.pow(raio, 2);
+//Math.pow para esponenciaçao de um numero 
+
+
+console.log( "A=", area.toFixed(4));
+//toFixed deixa a saida com 4 casas decimais 
