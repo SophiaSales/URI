0c5693707c134f155aa0ab15fbf51e2eb807eef4
SophiaSales <sophiabrenda10@gmail.com>
2021-04-10 15:22:02 -0300

    change

--- day3/ex02.js
+++ day3/ex02.js
@@ -6,6 +6,6 @@ var raio = parseFloat(input);
 var area = n * Math.pow(raio, 2);
 //Math.pow para esponenciaçao de um numero 
 
-
+console.log(typeof area);
 console.log( "A=", area.toFixed(4));
 //toFixed deixa a saida com 4 casas decimais 
--- index.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
-
-/**
- * Escreva a sua solução aqui
- * Code your solution here
- * Escriba su solución aquí
- */
-console.log( typeof lines);
