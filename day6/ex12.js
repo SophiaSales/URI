8c8245d5ac95a195b7a05ed8f32b311887c571b3
SophiaSales <sophiabrenda10@gmail.com>
2021-04-12 20:13:00 -0300

    day6 started

---
+++ day6/ex12.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+var[A, B, C] = input.split(" ").map(item => parseFloat(item));
+// map um mapeamento do array irar fazer um parseamento dos itens para float
+var pi = 3.14159;
+
+var triangulo = (A * C) / 2.0;
+var circulo = pi *Math.pow(C, 2);
+var trapezio = ((A + B) * C) / 2.0;
+var quadrado = B * B;
+var retangulo = A * B;
+
+console.log("TRIANGULO: "+ triangulo.toFixed(3));
+console.log("CIRCULO: "+ circulo.toFixed(3));
+console.log("TRAPEZIO: "+ trapezio.toFixed(3));
+console.log("QUADRADO: "+ quadrado.toFixed(3));
+console.log("RETANGULO: "+ retangulo.toFixed(3));
