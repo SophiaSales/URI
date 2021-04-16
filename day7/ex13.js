30ac5e5bffa0fad213e5a9d9e4496b9cc7e4854b
SophiaSales <sophiabrenda10@gmail.com>
2021-04-16 16:46:57 -0300

    day7 started

---
+++ day7/ex13.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+const [A, B, C] = input.split(" ").map(item => parseInt(item));
+
+const maiorAB = (A + B + Math.abs(A-B)) / 2;
+const maiorC =(C + maiorAB + Math.abs(C - maiorAB)) / 2;
+
+console.log(maiorC + " eh o maior");
